75983b9f466089369d725d866118ff7c27e930e2
Yagafarov <dinmuhammadyagafarov@gmail.com>
2025-07-30 16:30:33 -0700

    yuklash

---
+++ js/textStrings.js
@@ -0,0 +1,208 @@
+///////////////////////////////////
+///// TEXT STRINGS ////////////////
+///////////////////////////////////
+
+var textStrings_EN = {
+    // Act 1
+    "niceHat"              : "OOH NICE HAT"                                          ,
+    "outtaHere"            : "yeah git on outta here"                                ,
+    "getARoom"             : "GROSS, GO GET A ROOM"                                  ,
+    "notCoolAnymore"       : "nvm hats aren't cool anymore"                          ,
+    "tvOnTv"               : "A TV... ON TV!"                                        ,
+    "cricky"               : "LIL' CRICKY <3"                                        ,
+    "tooManyCrickets"      : "okay that's too many crickets"                         ,
+    "normalPeep"           : "just a normal peep"                                    ,
+    "normalPeeps"          : "just some normal peeps"                                ,
+    "wowNothing"           : "WOWWEE, IT'S NOTHING"                                  ,
+    
+    // Act 2
+    "crazySquareAttacks"   : "CRAZED SQUARE ATTACKS"                                 ,
+    "justMissed"           : "oooooh just missed it"                                 ,
+    "somethingInteresting" : "(ya gotta catch 'em doing *something* interesting...)" ,
+    "whoIsScreaming"       : "(ya gotta catch who's screaming at 'em)"               ,
+    "circleFearsSquares"   : "CIRCLE FEARS SQUARES"                                  ,
+    "whoScaresThem"        : "(ya gotta also catch *who* they're scared by)"         ,
+    "areTheyScared"        : "(ya gotta catch 'em *being* scared by a square)"       , // not used
+    "squaresSnubCircles"   : "SQUARES SNUB CIRCLES"                                  ,
+    "areTheySnubbed"       : "(ya gotta catch 'em *while* snubbing a circle)"        , // not used
+    "everyoneHates"        : "EVERYONE HATES EVERYONE!!1!"                           ,
+    "almostEveryoneHates"  : "ALMOST EVERYONE HATES EVERYONE..."                     ,
+    "squaresHateCircles"   : "SQUARES HATE CIRCLES"                                  ,
+    "circlesHateSquares"   : "CIRCLES HATE SQUARES"                                  ,
+    "areTheyYelling"       : "(ya gotta catch 'em *yelling* at others)"              , // not used
+    "nerdsNow"             : "what are these nerds doing now"                        ,
+    "schockedPeep"         : "why's this peep shocked?"                              ,
+    "whatever"             : "whatever"                                              ,
+    
+    // Act 3
+    "ellipsis"             : ". . ."                                                 ,
+    "coolNoMore"           : "hats ain't cool no more, haven't you heard?"           ,
+    "beScared"             : "BE SCARED. BE ANGRY."                                  ,
+    
+    // MANIFESTO
+    "manifesto" : [
+       //"as if you viewers want GOOD news"       ,
+       "who tunes in to watch *people get along?*",
+       "peace is boring. violence goes viral."    ,
+       //"peace is boring. conflict gets clicks." ,
+       "and every story needs a conflict, so..."  ,
+       //"...GIVE THE AUDIENCE WHAT THEY WANT."   ,
+       "GIVE THE AUDIENCE WHAT THEY WANT."
+    ] ,
+    
+    // -- Strings currently in graphics resources --
+    
+    // (bg_preload.png) Preload screen
+    "playingTime"        : "playing time: 5 minutes" ,
+    "warning"            : "warning: the following program\ncontains scenes of snobbery,\nrudeness & mass murder.\nviewer discretion is advised." ,
+    
+    // (cam-instructions.png) Camera Instructions
+    "pointAndClick"      : "" ,
+    
+    // (chyron3.png)
+    "chyronNothing"      : "WOWWEE, IT'S *NOTHING*" ,
+    
+    // (credits0001.png)
+    "createdBy"          : "created by" ,
+    "NickyCase"          : "Nicky Case" ,
+    
+    // (credits0002.png)
+    "manyThanks"         : "many thanks to my playtesters:" ,
+    
+    // (credits000[3,4,5,6].png)
+    "patreonSupporters"  : "and to my patreon supporters:" ,
+    
+    // (credits0007.png)
+    "lastButNotLeast"    : "and last but not least," ,
+    
+    // (credits0008.png)
+    "thankYouForPlaying" : "thank YOU for playing!" ,
+    
+    // (preload_play.png)
+    "playButton"         : "PLAY" ,
+    
+    // (end_button.png) Post Credits
+    "otherWorkButton"    : "see my other work" ,
+    "buyCoffeeButton"    : "buy me a coffee" ,
+    "replayButton"       : "replay this mess" ,
+    
+    // (logo.png) Post Credits
+    "logoWBWWB"          : "SHARE THE PAIN:" ,
+    
+    // (end_prototype.png)
+    "endOfPrototype"     : "END OF PROTOTYPE" ,
+    "toBeContinued"      : "(to be continued!)" ,
+    
+    // (quote0002.png)
+    "WBWWB"              : "We become what we behold." ,
+    "WSOTATOTSU"         : "We shape our tools and then our tools shape us." ,
+    
+    // (quote0003.png)
+    "MarshallMcLuhan"    : "Marshall McLuhan" ,
+    
+    // (quote0004.png)
+    "misatrributed"      : "(misattributed)" ,
+};
+
+var textStrings_UZ = {
+    // Act 1
+    "niceHat"             : "VOY, AJOYIB SHAPKA!",
+    "outtaHere"           : "Ha, tezroq yoʻqoling!",
+    "getARoom"            : "JIRKANCH! BORIB OʻZINGIZGA XONA OLINGLAR!",
+    "notCoolAnymore"      : "Nima boʻldi, shapkalar endi urfda emas.",
+    "tvOnTv"              : "TELEVIZOR... TELEVIZORDA!",
+    "cricky"              : "KICHKINTOY CHIGIRTKA <3",
+    "tooManyCrickets"     : "Boʻldi, bu juda koʻp chigirtka boʻldi.",
+    "normalPeep"          : "Shunchaki oddiy odam.",
+    "normalPeeps"         : "Shunchaki oddiy odamlar.",
+    "wowNothing"          : "VOY-BOʻY, BU HECH NIMA EMAS!",
+
+    // Act 2
+    "crazySquareAttacks"  : "JINNIDEK KVADRAT HUJUMLARI",
+    "justMissed"          : "Voy, sal qolibdi, oʻtkazib yubordingiz.",
+    "somethingInteresting": "(Ular qiziqarli nima qilayotganini topishingiz kerak...)",
+    "whoIsScreaming"      : "(Kim ularga baqirayotganini topishingiz kerak.)",
+    "circleFearsSquares"  : "DOIRA KVADRATLARDAN QOʻRQADI",
+    "whoScaresThem"       : "(Ularni kim qoʻrqitayotganini ham topishingiz kerak.)",
+    "areTheyScared"       : "(Ularni kvadratdan qoʻrqayotganini topishingiz kerak.)",
+    "squaresSnubCircles"  : "KVADRATLAR DOIRALARNI NAZARGA ILMAYDI",
+    "areTheySnubbed"      : "(Ularni doirani nazarga ilmayotganida topishingiz kerak.)",
+    "everyoneHates"       : "HAMMA HAMMADAN NAZAR YUZ OʻGIRADI!!1!",
+    "almostEveryoneHates" : "DEYARLI HAMMA HAMMADAN NAZAR YUZ OʻGIRADI...",
+    "squaresHateCircles"  : "KVADRATLAR DOIRALARNI YOQTIRMAYDI",
+    "circlesHateSquares"  : "DOIRALAR KVADRATLARNI YOQTIRMAYDI",
+    "areTheyYelling"      : "(Ularni boshqalarga baqirayotganini topishingiz kerak.)",
+    "nerdsNow"            : "Bu nerdlar hozir nima qilyapti?",
+    "schockedPeep"        : "Nega bu odam shokka tushgan?",
+    "whatever"            : "Baribir.",
+
+    // Act 3
+    "ellipsis"            : ". . .",
+    "coolNoMore"          : "Shapkalar endi zamonaviy emasligini eshitmadingizmi?",
+    "beScared"            : "QOʻRQING. JAHLLANING.",
+
+    // MANIFESTO
+    "manifesto" : [
+        "Axir tomoshabinlar YAXSHI xabarlarni xohlaydimi?",
+        "Kim odamlar bir-biri bilan kelishganini koʻrishni xohlaydi?",
+        "Tinchlik zerikarli. Zoʻravonlik virusdek tarqaladi.",
+        "Va har bir hikoyada ziddiyat boʻlishi kerak, shuning uchun...",
+        "TOMOSHABINGA ULAR NIMA XOHLASA, SHUNI BERING."
+    ] ,
+
+    // -- Strings currently in graphics resources --
+
+    // (bg_preload.png) Preload screen
+    "playingTime"         : "Oʻynash vaqti: 5 daqiqa",
+    "warning"             : "Ogohlantirish: quyidagi dasturda\nmanmanlik, qoʻpollik va ommaviy qirgʻin sahnalari mavjud.\nTomoshabin ehtiyot boʻlsin.",
+
+    // (cam-instructions.png) Camera Instructions
+    "pointAndClick"       : "NUQTANI BOSING",
+
+    // (chyron3.png)
+    "chyronNothing"       : "VOY-BOʻY, BU *HECH NIMA*!",
+
+    // (credits0001.png)
+    "createdBy"           : "Yaratuvchisi:",
+    "NickyCase"           : "Nicky Case",
+
+    // (credits0002.png)
+    "manyThanks"          : "Sinovchilarimga katta rahmat:",
+
+    // (credits000[3,4,5,6].png)
+    "patreonSupporters"   : "Va Patreon homiylarimga ham:",
+
+    // (credits0007.png)
+    "lastButNotLeast"     : "Va nihoyat,",
+
+    // (credits0008.png)
+    "thankYouForPlaying" : "Oʻynaganingiz uchun rahmat!",
+
+    // (preload_play.png)
+    "playButton"          : "OʻYNA",
+
+    // (end_button.png) Post Credits
+    "otherWorkButton"     : "Boshqa ishlarimni koʻring",
+    "buyCoffeeButton"     : "Menga kofe olib bering",
+    "replayButton"        : "Bu tartibsizlikni qayta oʻynash",
+
+    // (logo.png) Post Credits
+    "logoWBWWB"           : "OGʻRIQNI ULASHING:",
+
+    // (end_prototype.png)
+    "endOfPrototype"      : "PROTOTIP YAKUNI",
+    "toBeContinued"       : "(davom etadi!)",
+
+    // (quote0002.png)
+    "WBWWB"               : "Biz nimani koʻrsak, oʻshanga aylanamiz.",
+    "WSOTATOTSU"          : "Biz asboblarimizni shakllantiramiz, keyin asboblarimiz bizni shakllantiradi.",
+
+    // (quote0003.png)
+    "MarshallMcLuhan"     : "Marshall McLuhan",
+
+    // (quote0004.png)
+    "misatrributed"       : "(notoʻgʻri nisbat berilgan)",
+};
+
+// O'zbek tilini tanlash uchun:
+var textStrings = textStrings_UZ;
